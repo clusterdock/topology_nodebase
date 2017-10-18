f0de771c6f7dae0a7175383493c3f36b9dc731bd
Dima Spivak <dimaspivak@users.noreply.github.com>
2017-10-18 09:58:14 -0700

    Update PR will minor style changes

--- start.py
+++ start.py
@@ -16,22 +16,22 @@ import yaml
 
 from clusterdock.models import Cluster, Node
 
-logger = logging.getLogger('clusterdock.{}'.format(__name__))
-
 DEFAULT_NAMESPACE = 'clusterdock'
 DEFAULT_OPERATING_SYSTEM = 'centos6.6'
 
+logger = logging.getLogger('clusterdock.{}'.format(__name__))
+
 
 def main(args):
-    if args.node_disks:
-        node_disks = yaml.load(args.node_disks)
-        logger.debug(node_disks)
-
     image = '{}/{}/topology_nodebase:{}'.format(args.registry,
                                                 args.namespace or DEFAULT_NAMESPACE,
                                                 args.operating_system or DEFAULT_OPERATING_SYSTEM)
 
+    if args.node_disks:
+        node_disks = yaml.load(args.node_disks)
+        logger.debug('Parsed node disks: %s.', node_disks)
+
     cluster = Cluster(*[Node(hostname=hostname, group='nodes', image=image,
-                             devices=node_disks.get(hostname) if args.node_disks else [])
+                             devices=node_disks.get(hostname) if args.node_disks else None)
                         for hostname in args.nodes])
     cluster.start(args.network)
